5d3f6936dbec67db06c848ec6bcbbd95442ba009
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-21 15:29:03 +0300

    Добавил ключи...

---
+++ src/fetchGallery.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+export { fetchGallery };
+    
+    
+    async function fetchImage() {
+    const BASE_URL = `https://pixabay.com/api/`;
+        const KEY = `30076693-e53e479545b9ac001fe6f9d5f`;
+        
+        
+    }
+
+// fetch(
+//     'https://pixabay.com/api/?key=30076693-e53e479545b9ac001fe6f9d5ff&q=yellow+flowers&image_type=photo'
+// )
+//     .then(r => r.json())
+//     .then(console.log);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { fetchGallery } from './fetchGallery';
+
